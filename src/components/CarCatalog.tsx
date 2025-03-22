96299b8c84ae0d308c90f7df946f79e83aa4e56f
Ratapapop Sarapap <popeangping1815@gmail.com>
2025-03-22 16:57:41 +0700

    productcard

--- src/components/CarCatalog.tsx
+++ src/components/CarCatalog.tsx
@@ -1,22 +1,32 @@
 import Link from "next/link";
 import ProductCard from "./ProductCard";
-import { CarItem,CarJson } from "interfaces";
+import { CarItem, CarJson } from "interfaces";
 
-export default async function CarCatalog({carJson}:{carJson:CarJson}){
-    const carJsonReady = await carJson;
-    return (
-        <>
-            Explore {carJsonReady.data.length} models in our catalog.
-            <div style={{margin:"20px", display:"flex", flexDirection:"row", flexWrap:"wrap", justifyContent:"space-around", alignContent:"space-around"}}>
-               {
-                carJsonReady.data.map((carItem:CarItem)=>(
-                    <Link href={`/car/${carItem.id}`}
-                    className="w-[100%] sm:w-[50%] md:w-[30%] lg:w-[25%] p-2 sm:p-4 md:p-4 lg:p-8">
-                        <ProductCard Name={carItem.name} imgSrc={carItem.picture}/>
-                    </Link>
-                ))
-               }
+export default async function CarCatalog({ carJson }: { carJson: CarJson }) {
+  const carJsonReady = await carJson;
+  return (
+    <>
+      <div className="text-3xl font-[Verdana,Geneva,Tahoma,sans-serif] text-center mb-8 text-[#333] text-[#FFF2F2]">
+        Explore {carJsonReady.data.length} models in our catalog.
+      </div>
+      <div className="flex flex-wrap justify-center gap-8 px-4">
+        {carJsonReady.data.map((carItem: CarItem) => (
+          <Link
+            href={`/car/${carItem.id}`}
+            key={carItem.id}
+            className="w-full sm:w-[48%] md:w-[30%] lg:w-[22%] p-2 sm:p-4 md:p-4 lg:p-8 group"
+          >
+            <div className="border border-gray-300 rounded-lg overflow-hidden shadow-lg bg-white relative group hover:scale-105 transition-all ease-in-out">
+              <ProductCard Name={carItem.name} imgSrc={carItem.picture} />
+
+              <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-40 transition-opacity"></div>
+              <div className="absolute inset-0 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity">
+                <span className="text-xl font-bold text-white">Explore</span>
+              </div>
             </div>
-        </>
-    );
+          </Link>
+        ))}
+      </div>
+    </>
+  );
 }
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,22 +1,42 @@
-import Image from 'next/image'
-import InteractiveCard from './InteractiveCard';
+import Image from "next/image";
+import InteractiveCard from "./InteractiveCard";
 
-export default function ProductCard({Name,imgSrc,onCompare}:{Name:string,imgSrc:string,onCompare?:Function}){
-    return (
-        <InteractiveCard contentName={Name}>    
-            <div className='w-full h-[70%] relative rounded-t-lg'>
-                <Image src={imgSrc}
-                alt ='Product Picture'
-                fill = {true}
-                className='object-cover rounded-t-lg'/>
-            </div>
-            <div className='w-full h-[15%] p-[10px]'>{Name}</div>
-            {
-                onCompare?<button className='block h-[10%] tex-sm rounded-md bg-sky-600 hover:bg-indigo-600 mx-2 px-1 py-1 text-white shadow-sm'
-                onClick={(e)=>{e.stopPropagation();e.preventDefault();onCompare(Name)}}>
-                    Compare
-                    </button> : ''
-            }
-        </InteractiveCard>
-    )
+export default function ProductCard({
+  Name,
+  imgSrc,
+  onCompare,
+}: {
+  Name: string;
+  imgSrc: string;
+  onCompare?: Function;
+}) {
+  return (
+    <InteractiveCard contentName={Name}>
+      <div className="w-full h-[70%] relative rounded-t-lg">
+        <Image
+          src={imgSrc}
+          alt="Product Picture"
+          fill={true}
+          className="object-cover rounded-t-lg"
+        />
+      </div>
+      <div className="w-full h-[15%] p-[10px] font-[Verdana,Geneva,Tahoma,sans-serif] text-black">
+        {Name}
+      </div>
+      {onCompare ? (
+        <button
+          className="block h-[10%] tex-sm rounded-md bg-sky-600 hover:bg-indigo-600 mx-2 px-1 py-1 text-white shadow-sm"
+          onClick={(e) => {
+            e.stopPropagation();
+            e.preventDefault();
+            onCompare(Name);
+          }}
+        >
+          Compare
+        </button>
+      ) : (
+        ""
+      )}
+    </InteractiveCard>
+  );
 }
