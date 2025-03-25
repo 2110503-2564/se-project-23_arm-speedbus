15a0dc8b05ed5c2e85deb46232aa58c1da908b80
Ratapapop Sarapap <popeangping1815@gmail.com>
2025-03-25 13:50:37 +0700

    loading

--- src/app/auditlog/page.tsx
+++ src/app/auditlog/page.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 import getAugitLogs from "@/libs/getAuditLogs";
 import dayjs from "dayjs";
-import { AuditLogJson} from "interfaces";
+import { AuditLogJson } from "interfaces";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -11,7 +11,7 @@ export default function AuditLogsPage() {
   const { data: session } = useSession();
   const [auditLogJson, setAuditLogJson] = useState<AuditLogJson>({
     success: false,
-    data: []
+    data: [],
   });
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
@@ -24,12 +24,12 @@ export default function AuditLogsPage() {
       try {
         const response = await getAugitLogs(session?.user.token);
         setAuditLogJson(response);
-        if(session.user.User_info.role!=='admin'){
-          setError("You are not an administrator. Access denied.")
-          return
+        if (session.user.User_info.role !== "admin") {
+          setError("You are not an administrator. Access denied.");
+          return;
         }
-        if(!response.success){
-            setError("Could not fetch audit logs.");
+        if (!response.success) {
+          setError("Could not fetch audit logs.");
         }
       } catch (err) {
         setError("Could not fetch audit logs.");
@@ -40,71 +40,91 @@ export default function AuditLogsPage() {
 
     fetchAuditLogs();
   }, [refresh]);
-  if (loading) return <div className="text-center text-xl text-black p-4 bg-slate-100 rounded-lg shadow-md max-w-md mx-auto">Loading...</div>;
-  if (error) return <div className="text-center text-xl text-red-600 p-4 bg-slate-100 rounded-lg shadow-md max-w-md mx-auto">{error}</div>
+  if (loading)
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <div className="w-10 h-10 border-4 border-t-blue-500 border-gray-300 rounded-full animate-spin"></div>
+      </div>
+    );
+  if (error)
+    return (
+      <div className="text-center text-xl text-red-600 p-4 bg-slate-100 rounded-lg shadow-md max-w-md mx-auto">
+        {error}
+      </div>
+    );
   return (
     <main className="p-6 min-h-screen font-sans">
-        {session.user.User_info.role==='admin'?
+      {session.user.User_info.role === "admin" ? (
         <div className="max-w-4xl mx-auto">
-         
           <div className="mb-8 text-center">
             <h1 className="text-4xl font-semibold text-gray-800 mb-2 font-poppins">
               All Audit Logs
             </h1>
             <h1 className="text-xl font-semibold text-gray-800 mb-2 font-poppins">
-            This platform has recorded {auditLogJson.data.length} actions so far.
+              This platform has recorded {auditLogJson.data.length} actions so
+              far.
             </h1>
             <h2 className="text-lg text-white font-open-sans">
-            Tracks all user actions, changes, and events for transparency and accountability.
+              Tracks all user actions, changes, and events for transparency and
+              accountability.
             </h2>
           </div>
-        
 
-        <div className="bg-white rounded-lg shadow overflow-hidden">
-          {auditLogJson?.data?.length === 0 ? (
-            <div className="p-6 text-center text-gray-500 font-open-sans">
-              No edit history found.
-            </div>
-          ) : (
-            <div className="divide-y divide-gray-200">
-              {auditLogJson?.data?.map((auditLogItem) => (
-                <div key={auditLogItem._id} className="p-6 font-open-sans">
-                  <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                    <div>
-                      <div className="text-lg font-semibold text-gray-700 font-poppins">
-                        Audit Log ID: {auditLogItem._id}
+          <div className="bg-white rounded-lg shadow overflow-hidden">
+            {auditLogJson?.data?.length === 0 ? (
+              <div className="p-6 text-center text-gray-500 font-open-sans">
+                No edit history found.
+              </div>
+            ) : (
+              <div className="divide-y divide-gray-200">
+                {auditLogJson?.data?.map((auditLogItem) => (
+                  <div key={auditLogItem._id} className="p-6 font-open-sans">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                      <div>
+                        <div className="text-lg font-semibold text-gray-700 font-poppins">
+                          Audit Log ID: {auditLogItem._id}
+                        </div>
+                        <div className="text-gray-600">
+                          Action: {auditLogItem.action}
+                        </div>
+                        <div className="text-gray-600">
+                          User ID: {auditLogItem.user_id._id}
+                        </div>
                       </div>
-                      <div className="text-gray-600">
-                        Action: {auditLogItem.action}
-                      </div>
-                      <div className="text-gray-600">
-                        User ID: {auditLogItem.user_id._id}
-                      </div>
-                    </div>
-                    <div>
-                      <div className="text-gray-600">
-                        Target: {auditLogItem.target}
-                      </div>
-                      <div className="text-gray-600">
-                        Target ID: {auditLogItem.target_id} 
-                      </div>
-                      <div className="text-gray-600">
-                        Description: {auditLogItem.description}
-                      </div>
-                      <div className="text-gray-600">
-                        Time Stamp: {auditLogItem.timeStamp}
+                      <div>
+                        <div className="text-gray-600">
+                          Target: {auditLogItem.target}
+                        </div>
+                        <div className="text-gray-600">
+                          Target ID: {auditLogItem.target_id}
+                        </div>
+                        <div className="text-gray-600">
+                          Description: {auditLogItem.description}
+                        </div>
+                        <div className="text-gray-600">
+                          Time Stamp: {auditLogItem.timeStamp}
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              ))}
-            </div>
-          )}
+                ))}
+              </div>
+            )}
+          </div>
         </div>
-      </div>:<div className="text-center text-xl text-red-600 p-4 bg-slate-100 rounded-lg shadow-md max-w-md mx-auto">
+      ) : (
+        <div className="text-center text-xl text-red-600 p-4 bg-slate-100 rounded-lg shadow-md max-w-md mx-auto">
           You are not an administrator. Access denied.
         </div>
-    }
+      )}
     </main>
   );
 }
