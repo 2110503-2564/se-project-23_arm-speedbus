760a546bc557af3a4a5e77bb6ace9c42145f5999
Mantawit Ketphan <mantawit999@gmail.com>
2025-04-15 09:48:30 +0700

    Add function get coupon

---
+++ src/libs/getCouponByUserId.tsx
@@ -0,0 +1,12 @@
+export default async function getCouponsByUserId(token:string, id:string){
+    // await new Promise((resolve)=>{setTimeout(resolve,5000);})
+    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/rewards/${id}`,{
+        cache:"no-store",
+        method:"GET",
+        headers:{
+            authorization:`Bearer ${token}`,
+            "Content-type":"application/json"
+        }
+    });
+    return await response.json();
+}
