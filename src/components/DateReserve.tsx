e5ab1f48abfa8dbc87d605e2211b1ae2b464ec63
Ratapapop Sarapap <popeangping1815@gmail.com>
2025-04-18 23:18:17 +0700

    fix cant booking

--- src/components/DateReserve.tsx
+++ src/components/DateReserve.tsx
@@ -1,22 +1,40 @@
-'use client'
+"use client";
 import { DatePicker } from "@mui/x-date-pickers";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { Select, MenuItem} from "@mui/material"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Dayjs } from "dayjs";
 
-export default function DateReserve({value,onDateChange,label}:{value?:Dayjs|null,onDateChange:Function,label?: string;}){
-    const [date,setDate] = useState<Dayjs|null>(null);
-    const [location,setLocation] = useState('BKK');
-    if(value){
-        setDate(value);
+export default function DateReserve({
+  value,
+  onDateChange,
+  label,
+}: {
+  value?: Dayjs | null;
+  onDateChange: (date: Dayjs | null) => void;
+  label?: string;
+}) {
+  const [date, setDate] = useState<Dayjs | null>(null);
+
+  useEffect(() => {
+    if (value) {
+      setDate(value);
     }
-    return (
-        <div className="space-x-5 w-fit py-3 flex flex-row justify-center">
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker className="bg-white" label={label} value={date} onChange={(value)=>{setDate(value);onDateChange(value)}}/>
-            </LocalizationProvider>
-        </div>
-    );
+  }, [value]);
+
+  return (
+    <div className="space-x-5 w-fit py-3 flex flex-row justify-center">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          className="bg-white"
+          label={label}
+          value={date}
+          onChange={(newDate) => {
+            setDate(newDate);
+            onDateChange(newDate);
+          }}
+        />
+      </LocalizationProvider>
+    </div>
+  );
 }
