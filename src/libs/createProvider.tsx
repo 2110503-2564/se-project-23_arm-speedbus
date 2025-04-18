ab774f01fe24bb402b48612c6db500899dd63739
Baworntatt Taeprasert <baworntatt.t@gmail.com>
2025-04-18 15:11:57 +0700

    add and edit libs about coupon

--- src/libs/createProvider.tsx
+++ src/libs/createProvider.tsx
@@ -1,20 +1,29 @@
-export default async function createProvider(token:string,name:string,address:string,tel:string,email:string,picture:string,openTime:string,closeTime:string){
-    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/providers`,{
-        cache:"no-store",
-        method:"POST",
-        headers:{
-            authorization:`Bearer ${token}`,
-            "Content-type":"application/json"
-        },
-        body:JSON.stringify({
-            name:name,
-            address:address,
-            tel:tel,
-            email:email,
-            picture:picture,
-            openTime:openTime,
-            closeTime:closeTime
-        })
-    });
-    return await response.json();
+export default async function createProvider(
+  token: string,
+  name: string,
+  address: string,
+  tel: string,
+  email: string,
+  picture: string,
+  openTime: string,
+  closeTime: string
+) {
+  const response = await fetch(`${process.env.BACKEND_URL}/api/v1/providers`, {
+    cache: "no-store",
+    method: "POST",
+    headers: {
+      authorization: `Bearer ${token}`,
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      address: address,
+      tel: tel,
+      email: email,
+      picture: picture,
+      openTime: openTime,
+      closeTime: closeTime,
+    }),
+  });
+  return await response.json();
 }
--- src/libs/finishRent.tsx
+++ src/libs/finishRent.tsx
@@ -1,12 +1,15 @@
-export default async function finishRent(token:string,id:string){
-    // await new Promise((resolve)=>{setTimeout(resolve,5000);})
-    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/rents/finish/${id}`,{
-        cache:"no-store",
-        method:"PUT",
-        headers:{
-            authorization:`Bearer ${token}`,
-            "Content-type":"application/json"
-        }
-    });
-    return await response.json();
+export default async function finishRent(token: string, id: string) {
+  // await new Promise((resolve)=>{setTimeout(resolve,5000);})
+  const response = await fetch(
+    `${process.env.BACKEND_URL}/api/v1/rents/finish/${id}`,
+    {
+      cache: "no-store",
+      method: "PUT",
+      headers: {
+        authorization: `Bearer ${token}`,
+        "Content-type": "application/json",
+      },
+    }
+  );
+  return await response.json();
 }
