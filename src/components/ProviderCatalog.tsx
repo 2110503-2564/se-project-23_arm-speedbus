f0efdce5dd222e180fb7def20e89165c180b9a45
Ratapapop Sarapap <popeangping1815@gmail.com>
2025-04-18 23:48:38 +0700

    fix

--- src/components/ProviderCatalog.tsx
+++ src/components/ProviderCatalog.tsx
@@ -4,6 +4,7 @@ import ProductCard from "./ProductCard";
 import { ProviderItem, ProviderJson } from "interfaces";
 
 import { FaFilter } from "react-icons/fa";
+import ProviderCard from "./ProviderCard";
 
 export default async function ProviderCatalog({
   providerJson,
@@ -13,15 +14,12 @@ export default async function ProviderCatalog({
   const providerJsonReady = await providerJson;
   return (
     <>
-
-      <div className="flex text-3xl font-[Verdana,Geneva,Tahoma,sans-serif] text-left mt-4 mb-8 text-[#333] opacity-0 transition-opacity duration-1000 animate-fade-in ml-20 pl-10">
+      <div className="flex text-3xl font-robotoMono text-left mt-4 mb-8 text-[#333] opacity-0 transition-opacity duration-1000 animate-fade-in ml-20 pl-10">
         Provider All_
       </div>
 
-      <div className="flex text-m font-[Verdana,Geneva,Tahoma,sans-serif] text-left mt-4 mb-8 text-[#333] opacity-0 transition-opacity duration-1000 animate-fade-in pl-20 ml-10 mr-10 pr-20">
-        <div className="flex-1">
-          _ items
-        </div>
+      <div className="flex text-[15px]  text-left mt-24 mb-8 text-[#808080] opacity-0 transition-opacity duration-1000 animate-fade-in pl-20 ml-10 mr-10 pr-20">
+        <div className="flex-1  font-robotoMono ">_ ITEMS</div>
       </div>
 
       <div className="flex flex-wrap justify-center gap-8 px-4">
@@ -33,9 +31,11 @@ export default async function ProviderCatalog({
             style={{ animationDelay: `${index * 0.15}s` }}
           >
             <div className="border border-gray-300  overflow-hidden shadow-lg bg-white relative group hover:scale-105 transition-all ease-in-out">
-              <ProductCard
-                Name={providerItem.name}
+              <ProviderCard
+                providerName={providerItem.name}
                 imgSrc={providerItem.picture}
+                email={providerItem.email}
+                phoneNum={providerItem.tel}
               />
               <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-40 transition-opacity"></div>
               <div className="absolute inset-0 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity">
--- src/components/ProviderCatalogMain.tsx
+++ src/components/ProviderCatalogMain.tsx
@@ -4,28 +4,39 @@ import ProviderCard from "./ProviderCard";
 import { ProviderItem, ProviderJson } from "interfaces";
 const MAX_PROVIDERS_DISPLAYED = 3;
 
-export default async function ProviderCatalogMain({ providerJson }: { providerJson: ProviderJson }) {
+export default async function ProviderCatalogMain({
+  providerJson,
+}: {
+  providerJson: ProviderJson;
+}) {
   const providerJsonReady = await providerJson;
 
   return (
     <>
-      <div className="flex flex-wrap justify-center gap-8 px-4"> 
-        {providerJsonReady.data.slice(0, Math.min(providerJsonReady.data.length, 3)).map((providerItem: ProviderItem, index) => (
-          <Link
-            href={`/provider/${providerItem.id}`}
-            key={providerItem.id}
-            className={`w-full sm:w-[48%] md:w-[30%] lg:w-[22%] p-2 sm:p-4 md:p-4 lg:p-8 group opacity-0 transition-transform duration-1000 animate-slide-up`}
-            style={{ animationDelay: `${index * 0.15}s` }}
-          >
-            <div className="border border-gray-300  overflow-hidden shadow-lg bg-white relative group hover:scale-105 transition-all ease-in-out">
-              <ProviderCard providerName={providerItem.name} imgSrc={providerItem.picture} email={providerItem.email} phoneNum={providerItem.tel}/>
-              <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-40 transition-opacity"></div>
-              <div className="absolute inset-0 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity">
-                <span className="text-xl font-bold text-white">Explore</span>
+      <div className="flex flex-wrap justify-center gap-8 px-4">
+        {providerJsonReady.data
+          .slice(0, Math.min(providerJsonReady.data.length, 3))
+          .map((providerItem: ProviderItem, index) => (
+            <Link
+              href={`/provider/${providerItem.id}`}
+              key={providerItem.id}
+              className={`w-full sm:w-[48%] md:w-[30%] lg:w-[22%] p-2 sm:p-4 md:p-4 lg:p-8 group opacity-0 transition-transform duration-1000 animate-slide-up`}
+              style={{ animationDelay: `${index * 0.15}s` }}
+            >
+              <div className="border border-gray-300 h-auto overflow-hidden shadow-lg bg-white relative group hover:scale-105 transition-all ease-in-out">
+                <ProviderCard
+                  providerName={providerItem.name}
+                  imgSrc={providerItem.picture}
+                  email={providerItem.email}
+                  phoneNum={providerItem.tel}
+                />
+                <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-40 transition-opacity"></div>
+                <div className="absolute inset-0 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity">
+                  <span className="text-xl font-bold text-white">Explore</span>
+                </div>
               </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          ))}
       </div>
 
       <style jsx global>{`
