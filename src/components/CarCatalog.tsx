5757a45b83b4bfd8f883d60c272a7c07da62e496
Baworntatt Taeprasert <baworntatt.t@gmail.com>
2025-04-18 16:26:29 +0700

    fix productcard + change title pic

--- src/components/CarCatalog.tsx
+++ src/components/CarCatalog.tsx
@@ -10,20 +10,17 @@ export default async function CarCatalog({ carJson }: { carJson: CarJson }) {
   const carJsonReady = await carJson;
   return (
     <>
-       <div className="flex text-3xl font-[Verdana,Geneva,Tahoma,sans-serif] text-left mt-4 mb-8 text-[#333] opacity-0 transition-opacity duration-1000 animate-fade-in ml-20 pl-10">
-  Rent All_
-</div>
+      <div className="flex text-3xl font-[Verdana,Geneva,Tahoma,sans-serif] text-left mt-4 mb-8 text-[#333] opacity-0 transition-opacity duration-1000 animate-fade-in ml-20 pl-10">
+        Rent All_
+      </div>
 
-<div className="flex text-m font-[Verdana,Geneva,Tahoma,sans-serif] text-left mt-4 mb-8 text-[#333] opacity-0 transition-opacity duration-1000 animate-fade-in pl-20 ml-10 mr-10 pr-20">
-  <div className="flex-1">
-    _ items
-  </div>
-  <div className="flex-1 flex justify-end items-center gap-2">
-    <span>Filter</span>
-    <FaFilter />
-  </div>
-</div>
-
+      <div className="flex text-m font-[Verdana,Geneva,Tahoma,sans-serif] text-left mt-4 mb-8 text-[#333] opacity-0 transition-opacity duration-1000 animate-fade-in pl-20 ml-10 mr-10 pr-20">
+        <div className="flex-1">_ items</div>
+        <div className="flex-1 flex justify-end items-center gap-2">
+          <span>Filter</span>
+          <FaFilter />
+        </div>
+      </div>
 
       <div className="flex flex-wrap justify-center gap-4 px-4">
         {carJsonReady.data.map((carItem: CarItem, index) => (
@@ -34,7 +31,12 @@ export default async function CarCatalog({ carJson }: { carJson: CarJson }) {
             style={{ animationDelay: `${index * 0.15}s` }}
           >
             <div className="overflow-hidden bg-white relative group hover:scale-105 transition-all ease-in-out">
-              <ProductCard Name={carItem.name} imgSrc={carItem.picture} price={carItem.pricePerDay} provider={carItem.provider_info.name} />
+              <ProductCard
+                Name={carItem.name}
+                imgSrc={carItem.picture}
+                price={carItem.pricePerDay}
+                provider={carItem.provider_info.name}
+              />
               <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-40 transition-opacity"></div>
               <div className="absolute inset-0 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity">
                 <span className="text-xl font-bold text-white">Explore</span>
