009fb2dbcb1870584d8d46f28fd335ec6cfbb983
Chandan Kanyarat <6733029621@student.chula.ac.th>
2025-04-18 20:24:15 +0700

    Modified

--- src/components/ProviderCatalogMain.tsx
+++ src/components/ProviderCatalogMain.tsx
@@ -26,18 +26,6 @@ export default async function ProviderCatalogMain({ providerJson }: { providerJs
             </div>
           </Link>
         ))}
-        <Link
-          href={`/provider`}
-          className={`w-full sm:w-[48%] md:w-[30%] lg:w-[22%] p-2 sm:p-4 md:p-4 lg:p-8 group opacity-0 transition-transform duration-1000 animate-slide-up`}
-          style={{ animationDelay: `${MAX_PROVIDERS_DISPLAYED * 0.15}s` }}
-        >
-          <div className="border border-gray-300 rounded-lg overflow-hidden shadow-lg bg-white relative group hover:scale-105 transition-all ease-in-out">
-            <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-40 transition-opacity"></div>
-            <div className="absolute inset-0 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity">
-              <span className="text-xl font-bold text-white">Explore All Providers</span>
-            </div>
-          </div>
-        </Link>
       </div>
 
       <style jsx global>{`
