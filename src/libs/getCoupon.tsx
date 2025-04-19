0c58ab1b9a4d107d58ce29365718d23c1d60b77e
Baworntatt Taeprasert <baworntatt.t@gmail.com>
2025-04-19 21:09:54 +0700

    add libs

---
+++ src/libs/getCoupon.tsx
@@ -0,0 +1,15 @@
+export default async function getCoupons(token: string, id: string) {
+  // await new Promise((resolve)=>{setTimeout(resolve,5000);})
+  const response = await fetch(
+    `${process.env.BACKEND_URL}/api/v1/coupons/${id}`,
+    {
+      cache: "no-store",
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${token}`,
+        "Content-type": "application/json",
+      },
+    }
+  );
+  return await response.json();
+}
