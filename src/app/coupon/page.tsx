0a39e8d62cac94f216f2d916017374e8f2263842
Ratapapop Sarapap <popeangping1815@gmail.com>
2025-04-19 19:39:14 +0700

    merge

--- src/app/coupon/page.tsx
+++ src/app/coupon/page.tsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 import CouponCardItem from "@/components/CouponCardItem"; // adjust the path if necessary
 import getCouponTemplates from "@/libs/getCouponTemplates"; // make sure the path is correct
 import { CouponTemplateItem } from "interfaces";
+import SpendingMilestoneBar from "@/components/milestonebar";
 
 export default function Page() {
   const [couponList, setCouponList] = useState([]);
