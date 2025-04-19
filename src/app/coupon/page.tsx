f731ff9d32d0e923273cfc783105ca68eca1b9cb
Mantawit Ketphan <mantawit999@gmail.com>
2025-04-20 00:34:35 +0700

    Add totalPaymentThisYear

--- src/app/coupon/page.tsx
+++ src/app/coupon/page.tsx
@@ -5,16 +5,18 @@ import CouponCardItem from "@/components/CouponCardItem"; // adjust the path if 
 import getCouponTemplates from "@/libs/getCouponTemplates"; // make sure the path is correct
 import { CouponTemplateItem } from "interfaces";
 import SpendingMilestoneBar from "@/components/milestonebar";
+import { useSession } from "next-auth/react";
 
 export default function Page() {
   const [couponList, setCouponList] = useState([]);
+  const { data : session } = useSession();
 
   useEffect(() => {
     const fetchCouponData = async () => {
       try {
         // ดึงข้อมูลคูปองจาก API โดยไม่ใช้ token
         const coupons = await getCouponTemplates(); // No token required
-
+        console.log(JSON.stringify(session?.user.User_info, null, 2));
         // ตรวจสอบว่าได้รับข้อมูลคูปองจาก API
         if (coupons?.data) {
           setCouponList(coupons.data); // เก็บข้อมูลที่ได้จาก API
@@ -29,5 +31,5 @@ export default function Page() {
     fetchCouponData();
   }, []);
 
-  return <SpendingMilestoneBar currentSpending={2000} coupon={couponList} />;
+  return <SpendingMilestoneBar currentSpending={session?.user.User_info.totalPaymentThisYear as number} coupon={couponList} />;
 }
