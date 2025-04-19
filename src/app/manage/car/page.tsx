0fda8cb0db85ccfd336cfca9d9e3ced3d0253605
Mantawit Ketphan <mantawit999@gmail.com>
2025-04-19 17:12:33 +0700

    fix manage

--- src/app/manage/car/page.tsx
+++ src/app/manage/car/page.tsx
@@ -6,7 +6,7 @@ export default async function manageCarsPage() {
   return (
     <>
       {session?.user.User_info.role === "admin" ? (
-        <div className="bg-slate-100 p-6 rounded-lg shadow-md max-w-md mx-auto mt-10">
+        <div className="bg-white p-6 rounded-lg max-w-md mx-auto my-10">
           <h2 className="text-2xl font-semibold mb-6 text-indigo-800 text-center">
             Manage Cars
           </h2>
