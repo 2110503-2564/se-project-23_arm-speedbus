648a67fbe0d9f896060155af534cdf7f515ff292
Jaturapat Thanaboonthawee <jaturaphat078@gmail.com>
2025-04-26 14:38:02 +0700

    Add comfirmation message

--- src/components/MyReviewCard.tsx
+++ src/components/MyReviewCard.tsx
@@ -28,11 +28,15 @@ function renderStars(ratingScore: number) {
 export default function MyReviewCard({
   rating,
   editingId,
-  onSelect
+  onSelect,
+  setConfirmationMessage,
+  setColor,
 }: {
   rating: Rating;
   editingId: string | null;
   onSelect: (selectedRatingId: string | null) => void;
+  setConfirmationMessage: (message: string | null) => void;
+  setColor: (color: string) => void;
 }) {
   const router = useRouter();
   const { data: session } = useSession();
@@ -46,7 +50,7 @@ export default function MyReviewCard({
 
   const handleEdit = async () => {
     if (!session) return;
-    if(rating.car_rating === carRating && rating.review === review) return;
+    if (rating.car_rating === carRating && rating.review === review) return;
 
     const response = await updateRating(
       session.user.token,
@@ -54,20 +58,30 @@ export default function MyReviewCard({
       carRating,
       rating.provider_rating,
       review
-    )
+    );
 
     router.push("/myReview");
-  }
+
+    if (response.success) {
+      setColor("text-green-600");
+      setConfirmationMessage("Review updated successfully!");
+      setTimeout(() => setConfirmationMessage(null), 3000);
+    }
+  };
 
   const handleDelete = async () => {
     if (!session) return;
 
-    const response = await deleteRating(
-      session.user.token,
-      rating._id
-    )
+    const response = await deleteRating(session.user.token, rating._id);
 
+    setIsModalOpen(false);
     router.push("/myReview");
+
+    if (response.success) {
+      setColor("text-red-600");
+      setConfirmationMessage("Review deleted successfully!");
+      setTimeout(() => setConfirmationMessage(null), 3000);
+    }
   };
 
   return (
@@ -75,12 +89,10 @@ export default function MyReviewCard({
       <div className="flex flex-col">
         <div className="flex">
           <div className="text-black text-[16px]">RentId : {rating.rent_info}</div>
-          {
-            editingId === rating._id ? (
-              <div className="flex justify-end ml-auto">
-                {[1, 2, 3, 4, 5].map((star) => (
-                  (hoverRating || carRating) >= star
-                  ?
+          {editingId === rating._id ? (
+            <div className="flex justify-end ml-auto">
+              {[1, 2, 3, 4, 5].map((star) =>
+                (hoverRating || carRating) >= star ? (
                   <FaStar
                     key={star}
                     size={20}
@@ -89,7 +101,7 @@ export default function MyReviewCard({
                     onMouseEnter={() => setHoverRating(star)}
                     onMouseLeave={() => setHoverRating(0)}
                   />
-                  :
+                ) : (
                   <FaRegStar
                     key={star}
                     size={20}
@@ -98,12 +110,12 @@ export default function MyReviewCard({
                     onMouseEnter={() => setHoverRating(star)}
                     onMouseLeave={() => setHoverRating(0)}
                   />
-                ))}
-              </div>
-            ) : (
-              <div className="ml-auto">{renderStars(rating.car_rating)}</div>
-            )
-          }
+                )
+              )}
+            </div>
+          ) : (
+            <div className="ml-auto">{renderStars(carRating)}</div>
+          )}
         </div>
         <span className="text-black text-[16px]">Car : {rating.car_info.name}</span>
       </div>
@@ -120,7 +132,8 @@ export default function MyReviewCard({
 
           <div className="flex flex-row items-center justify-between mt-2">
             <div className="text-gray-500 text-[12px]">
-              Posted on : {rating.createdAt.toLocaleString()} {rating.isEdited && <span className="text-gray-500 text-[12px]">(Edited)</span>}
+              Posted on : {rating.createdAt.toLocaleString()}{" "}
+              {rating.isEdited && <span className="text-gray-500 text-[12px]">(Edited)</span>}
             </div>
             <div className="flex gap-2">
               <button
@@ -129,13 +142,13 @@ export default function MyReviewCard({
                   handleEdit();
                   onSelect(null);
                 }}
-                >
-                  Confirm
-                </button>
-              <button 
+              >
+                Confirm
+              </button>
+              <button
                 className={`${buttonStyle} border-red-600 text-red-600 hover:bg-red-600 hover:text-white`}
                 onClick={() => {
-                  onSelect(null)
+                  onSelect(null);
                   setReview(rating.review);
                   setCarRating(rating.car_rating);
                 }}
@@ -151,18 +164,16 @@ export default function MyReviewCard({
 
           <div className="flex flex-row items-center justify-between mt-2">
             <div className="text-gray-500 text-[12px]">
-              Posted on : {rating.createdAt.toLocaleString()} {rating.isEdited && <span className="text-gray-500 text-[12px]">(Edited)</span>}
+              Posted on : {rating.createdAt.toLocaleString()}{" "}
+              {rating.isEdited && <span className="text-gray-500 text-[12px]">(Edited)</span>}
             </div>
             <div className="flex gap-2">
-              <button
-                className={buttonStyle}
-                onClick={() => onSelect(rating._id)}
-              >
+              <button className={buttonStyle} onClick={() => onSelect(rating._id)}>
                 Edit
               </button>
               <button
                 className={`${buttonStyle} border-red-600 text-red-600 hover:bg-red-600 hover:text-white`}
-                onClick={() => setIsModalOpen(true)} // Open the modal
+                onClick={() => {setIsModalOpen(true);}}
               >
                 Delete
               </button>
@@ -172,30 +183,30 @@ export default function MyReviewCard({
       )}
 
       {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="bg-white rounded-lg p-6 w-[300px] shadow-lg">
-            <h2 className="text-lg font-bold mb-4 text-black font-robotoMono">Confirm Deletion</h2>
-            <p className="text-sm text-black mb-6 font-robotoMono">
-              Are you sure you want to delete this review? This action cannot be
-              undone.
-            </p>
-            <div className="flex justify-end gap-4">
-              <button
-                className="px-3 py-1 bg-white border border-red-600 text-red-600 hover:bg-red-600 hover:text-white rounded-lg font-robotoMono transition duration-300"
-                onClick={handleDelete}
-              >
-                Delete
-              </button>
-              <button
-                className="px-3 py-1 bg-white border border-black text-black hover:bg-black hover:text-white rounded-lg font-robotoMono transition duration-300"
-                onClick={() => setIsModalOpen(false)}
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
+         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+           <div className="bg-white rounded-lg p-6 w-[300px] shadow-lg">
+             <h2 className="text-lg font-bold mb-4 text-black font-robotoMono">Confirm Deletion</h2>
+             <p className="text-sm text-black mb-6 font-robotoMono">
+               Are you sure you want to delete this review? This action cannot be
+               undone.
+             </p>
+             <div className="flex justify-end gap-4">
+               <button
+                 className="px-3 py-1 bg-white border border-red-600 text-red-600 hover:bg-red-600 hover:text-white rounded-lg font-robotoMono transition duration-300"
+                 onClick={handleDelete}
+               >
+                 Delete
+               </button>
+               <button
+                 className="px-3 py-1 bg-white border border-black text-black hover:bg-black hover:text-white rounded-lg font-robotoMono transition duration-300"
+                 onClick={() => setIsModalOpen(false)}
+               >
+                 Cancel
+               </button>
+             </div>
+           </div>
+         </div>
+       )}
     </div>
   );
 }
